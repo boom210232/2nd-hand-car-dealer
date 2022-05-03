17a85c85d39cdfe6f58f2729df4eb70365830998
boom210232 <chonchanok.che@ku.th>
2022-05-03 16:31:23 +0700

    Updates with foreignkey error

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from models.market import Market
 from matplotlib.pyplot import title
 from blackdesert.blackdesert_online import BdoStarter
 
-bdo_starter = BdoStarter
+bdo_starter = BdoStarter()
 
 print(bdo_starter.market_list().get_all_market_transaction())
--- models/item_detail.py
+++ models/item_detail.py
@@ -1,19 +1,19 @@
 from sqlalchemy import Integer, Column, Text
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import declarative_base
+# from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
-
-Base = declarative_base()
+from .base import Base
 
 
 class ItemDetail(Base):
     __tablename__ = "item_detail"
     item_id = Column(Integer, primary_key=True)
     item_name = Column(Text)
-    types = Column(Text)
+    material = Column(Text)
     label = Column(Text)
 
     market = relationship("Market", back_populates="market")
 
     def __repr__(self):
-        return f"ItemsDetail(item_id={self.item_id}, item_name={self.item_name}, types={self.types}, label={self.label})"
+        return f"ItemsDetail(item_id={self.item_id}, item_name={self.item_name}, types={self.material}, label={self.label})"
