3a887a2e040d3d2bbcab64c594fa77affaca5627
boom210232 <chonchanok.che@ku.th>
2022-05-07 18:07:33 +0700

    add get post delete method with api

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ trash_item = ItemDetail(items_id=500, item_name="Unknown", material="weapon", la
 # print(bdo_starter.item_list().add_item(trash_item))
 print(bdo_starter.item_list().get_item_by_items_id(500))
 
-print(bdo_starter.market_list().get_market_by_username("livernoob"))
-print(bdo_starter.market_list().get_market_by_username_id(2))
-
-print("-------------------------------------------------------------")
-new_arrival=Market(username_id=52,username="BillClinton",items_id=28)
-# print(bdo_starter.market_list().add_market(new_arrival))
-print(bdo_starter.market_list().get_market_by_username("Trumped"))
+# print(bdo_starter.market_list().get_market_by_username("livernoob"))
+# print(bdo_starter.market_list().get_market_by_username_id(2))
+#
+# print("-------------------------------------------------------------")
+# new_arrival=Market(username_id=52,username="BillClinton",items_id=28)
+# # print(bdo_starter.market_list().add_market(new_arrival))
+# print(bdo_starter.market_list().get_market_by_username("Trumped"))
 
 app = FastAPI()
 
@@ -30,3 +30,7 @@ app = FastAPI()
 # @app.post("/item-list/add-new-market-user")
 # async def root(user_id, username,item_id):
 #     return {"info": bdo_starter.market_list().add_market(Market(username_id=user_id,username=str(username),items_id=item_id))}
+
+# @app.delete("/item-detail/{id}")
+# async def root(id):
+#     return {"info": bdo_starter.item_list().delete_item_by_id(id)}
